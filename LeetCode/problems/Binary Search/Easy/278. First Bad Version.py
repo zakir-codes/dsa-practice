4431cf34959a439bf2a099e91285d7869175a377
Mohd Zakir Hussain <84516466+zakir-codes@users.noreply.github.com>
2025-10-11 22:08:49 +0530

    Create 278. First Bad Version.py

---
+++ LeetCode/problems/Binary Search/Easy/278. First Bad Version.py
@@ -0,0 +1,16 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        
+        l,r = 1, n
+
+        while l<r:
+            mid = (l+r)//2
+            if not isBadVersion(mid):
+                l = mid+1
+            else:
+                r=mid
+        return l
+
