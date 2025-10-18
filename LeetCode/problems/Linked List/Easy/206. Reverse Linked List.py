326c7642d778fc3086a54ca8cea71ad2d693e05f
Mohd Zakir Hussain <84516466+zakir-codes@users.noreply.github.com>
2025-10-18 21:42:43 +0530

    Add reverseList function for linked list
    Implement reverse linked list function using iteration.

---
+++ LeetCode/problems/Linked List/Easy/206. Reverse Linked List.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        ## solution 1
+        if not head:
+            return head
+        prev, cur, nex = None, head, head.next
+
+        while nex: # recheck
+            cur.next = prev
+            prev = cur
+            cur = nex
+            nex = nex.next
+        cur.next = prev
+
+        return cur
+
+
