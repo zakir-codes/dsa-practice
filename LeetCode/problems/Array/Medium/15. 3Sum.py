d0f62c505ec963089d00bc1d402fc673a1ae1758
Mohd Zakir Hussain <84516466+zakir-codes@users.noreply.github.com>
2025-11-09 18:21:19 +0530

    Implement 3Sum solution using three pointers

---
+++ LeetCode/problems/Array/Medium/15. 3Sum.py
@@ -0,0 +1,25 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        ## solution 1: 3 pointers Time O(N^2) Space O(N)
+        res = []
+        nums.sort()
+        n = len(nums)
+        for i in range(n):
+            if i>0 and nums[i-1]==nums[i]:
+                continue
+            j,k = i+1,n-1
+            while j<k:
+                num = nums[i] + nums[j]+nums[k]
+                if num==0:
+                    res.append([nums[i],nums[j],nums[k]])
+                    j+=1
+                    k-=1
+                    while j<k and nums[j-1]==nums[j]:
+                        j+=1
+                    while j<k and nums[k+1]==nums[k]:
+                        k-=1
+                elif num<0:
+                    j+=1
+                else:
+                    k-=1
+        return res
